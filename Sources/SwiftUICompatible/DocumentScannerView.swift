ef1bd234b05f882692f8cd8f4c8ad035193b48fa
Shunzhe <git@mszmagic.com>
2020-10-18 12:46:00 -0400

    Added document scanner

---
+++ Sources/SwiftUICompatible/DocumentScannerView.swift
@@ -0,0 +1,49 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Shunzhe Ma on R 2/10/18.
+//
+
+import SwiftUI
+import VisionKit
+
+@available(iOS 13.0, *)
+public struct DocumentScannerView: UIViewControllerRepresentable {
+
+    private var onScanCompleted: (VNDocumentCameraScan) -> Void
+    
+    public init(onScanCompleted: @escaping (VNDocumentCameraScan) -> Void) {
+        self.onScanCompleted = onScanCompleted
+    }
+    
+    public func makeCoordinator() -> Coordinator {
+        Coordinator(onScanCompleted: self.onScanCompleted)
+    }
+
+    public func makeUIViewController(context: UIViewControllerRepresentableContext<DocumentScannerView>) -> VNDocumentCameraViewController {
+        let scanner = VNDocumentCameraViewController()
+        scanner.delegate = context.coordinator
+        return scanner
+    }
+
+    public func updateUIViewController(_ uiViewController: VNDocumentCameraViewController, context: UIViewControllerRepresentableContext<DocumentScannerView>) {
+        return
+    }
+    
+    public class Coordinator: NSObject, VNDocumentCameraViewControllerDelegate {
+        
+        private var onScanCompleted: (VNDocumentCameraScan) -> Void
+        
+        public init(onScanCompleted: @escaping (VNDocumentCameraScan) -> Void) {
+            self.onScanCompleted = onScanCompleted
+        }
+        
+        public func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+            self.onScanCompleted(scan)
+            controller.dismiss(animated: true, completion: nil)
+        }
+        
+    }
+
+}
